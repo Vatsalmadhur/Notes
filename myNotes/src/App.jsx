3b1b11c964e7ae1b54bade11df4d35fdcc3eef54
Madhur <bhartimadhur21@gmail.com>
2023-03-27 13:36:25 +0530

    Bug Fixing

--- myNotes/src/App.jsx
+++ myNotes/src/App.jsx
@@ -1,27 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList/NoteList";
 import Search from "./components/Search/Search";
 import "./index.css";
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      text: "lorem ipsum dolor sit amet, consectet",
-      date: "2020-01-01",
-    },
-    {
-      id: nanoid(),
-      text: "lorem ",
-      date: "2020-01-01",
-    },
-    {
-      id: nanoid(),
-      text: "lorem ipsum",
-      date: "2020-01-01",
-    },
-  ]);
+  const [notes, setNotes] = useState([]);
   const addNote = (text) => {
     const date = new Date();
     const newNote = {
@@ -29,19 +13,37 @@ const App = () => {
       text: text,
       date: date.toLocaleDateString(),
     };
-    const newNotes = [...notes, newNote];
-    setNotes(newNotes);
+    // const newNotes = [...notes, newNote];
+    // setNotes(newNotes);
+    const savedNotes=JSON.parse(localStorage.getItem("react-notes-app"));
+    
+    const newNotes=savedNotes ? [...savedNotes,newNote] :[newNote];
+     localStorage.setItem("react-notes-app", JSON.stringify(newNotes))
+     setNotes(newNotes);
+     console.log("saved")
   };
   const delNotes = (id) => {
-    const newNotes = notes.filter((note) => 
-      note.id !== id
+    // const newNotes = notes.filter((note) => 
+    //   note.id !== id
+    const note=JSON.parse(localStorage.getItem("react-notes-app"));
+    const newNotes=note.filter((note) => note.id!==id);
+    localStorage.setItem("react-notes-app", JSON.stringify(newNotes)
+
     );
     setNotes(newNotes);
   };
 
   const [searchNote,setSearchNote]=useState('');
 
-
+  useEffect(()=>{
+    const savedNotes=JSON.parse(localStorage.getItem("react-notes-app"));
+    if(savedNotes){
+      setNotes(savedNotes);
+      console.log(savedNotes)
+    }
+    console.log("something")
+    
+  },[])
 
   return (
     <>
